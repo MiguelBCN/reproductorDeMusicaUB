298b0da8332acde9cf875b65be39269660a1a7fa
MiguelBCN <47951953+0nizukaGT0@users.noreply.github.com>
2020-02-25 11:45:00 +0100

    Menu added

--- ub/info/prog2/HuayllasMigueDiCore/vista/IniciadorAplicacioUB.java
+++ ub/info/prog2/HuayllasMigueDiCore/vista/IniciadorAplicacioUB.java
@@ -3,12 +3,8 @@ package ub.info.prog2.HuayllasMigueDiCore.vista;
 public class IniciadorAplicacioUB {
     public static void main(String[] args) {
 
-        AplicacioUB1 aplicacio;
+        ReproductorUB1 repro= new ReproductorUB1();
+        repro.gestionReproductorMusica();1
     }
-    class AplicacioUB1{
-        //Este meetodo define el comportamiento del proyecto
-        void gestioReproductorUB(){}
-        void IniciadorReproductorUB(){}
-    }
-    class FitxerMultimedia{}
+
 }
--- ub/info/prog2/HuayllasMigueDiCore/vista/ReproductorUB1.java
+++ ub/info/prog2/HuayllasMigueDiCore/vista/ReproductorUB1.java
@@ -1,4 +1,72 @@
 package ub.info.prog2.HuayllasMigueDiCore.vista;
 
+import ub.info.prog2.utils.Menu;
+
+import java.util.Scanner;
+
 public class ReproductorUB1 {
+    /*OPCIONES DEL MENU
+    1.Afegir fitxer multimèdia:
+2. Eliminar fitxer multimèdia.
+3. Mostrar llista:
+4. Guardar llista:
+5. Recuperar llista:
+6. Sortir:
+*/
+    //Declaro la variables del menu para hacer referencia a las opciones del menu
+    static private enum OpcionesMeu {
+        AGREGAR_FICHERO, ELIMINAR_FICHERO,
+        MOSTRAR_LISTA, GUARDAR_LISTA, RECUPERAR_LISTA, SALIR
+    };
+
+    //Declaro las descripciones de cada menu
+    static private String[] desOpcionesMenu = {
+            "Demandar datos del fichero y agregarlo a la lista de ficheros",
+            "Eliminar un fichero de la lista",
+            " Mostra el contenido de la lista de ficheros mostrando delante de cada  su numero de posicion comenzando por el 1"
+            , "Guardar el contenido de la lista en un fichero",
+            "Cargar una lista previamente guardada en un fichero",
+            "Salir de la aplicacion"};
+
+    public void gestionReproductorMusica(){
+        Scanner sc =new Scanner(System.in);
+        //Creamos el objeto Menu y le pasamos el enum con las opciones del Menu principal
+        Menu<OpcionesMeu> menu=new Menu<OpcionesMeu>("Menu Principal",OpcionesMeu.values());
+
+        //Asignamos las descripciones de las opciones
+        menu.setDescripcions(desOpcionesMenu);
+
+        //Obtenemos las opciones del menu y hacemos las acciones pertinentes
+        OpcionesMeu opcion=null;
+        do{
+            //Mostramos las opciones del menu
+            menu.mostrarMenu();
+
+            //Demandamos una opcion
+            opcion=menu.getOpcio(sc);
+
+            //Hacemos las acciones necesarias
+            switch (opcion){
+                case AGREGAR_FICHERO:
+                    System.out.println(desOpcionesMenu[0]);
+                    break;
+                case ELIMINAR_FICHERO:
+                    System.out.println(desOpcionesMenu[1]);
+                    break;
+                case MOSTRAR_LISTA:
+                    System.out.println(desOpcionesMenu[2]);
+                    break;
+                case GUARDAR_LISTA:
+                    System.out.println(desOpcionesMenu[3]);
+                    break;
+                case RECUPERAR_LISTA:
+                    System.out.println(desOpcionesMenu[4]);
+                    break;
+            }
+
+        }while (opcion!=OpcionesMeu.SALIR);
+
+    }
+
+
 }
