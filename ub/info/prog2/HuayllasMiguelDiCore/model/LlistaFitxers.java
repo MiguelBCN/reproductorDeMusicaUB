4522aca53001f8dee3b47fcf50c05cd3e88c3377
MiguelBCN <47951953+0nizukaGT0@users.noreply.github.com>
2020-02-25 13:41:50 +0100

    Implementado FitxersMultimedia
    Cosas que hemos visto en clase
    librerias abstractas
    herencia
    la clase Object la cual es general

--- ub/info/prog2/HuayllasMiguelDiCore/model/LlistaFitxers.java
+++ ub/info/prog2/HuayllasMiguelDiCore/model/LlistaFitxers.java
@@ -1,4 +1,40 @@
 package ub.info.prog2.HuayllasMiguelDiCore.model;
+import ub.info.prog2.utils.InFileList;
+import ub.info.prog2.utils.ReproException;
 
-public class LlistaFitxers {
+import java.io.File;
+import java.util.ArrayList;
+
+public class LlistaFitxers implements InFileList {
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void addFitxer(File file) throws ReproException {
+
+    }
+
+    @Override
+    public void removeFitxer(File file) {
+
+    }
+
+    @Override
+    public File getAt(int i) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
 }
+
+
